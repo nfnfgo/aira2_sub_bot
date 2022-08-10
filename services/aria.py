bd9a87061d924220263767b7e23918e67e15a1e9
Oyasuminasai <61616918+nfnfgo@users.noreply.github.com>
2022-08-10 14:47:59 +0800

    Save

--- services/aria.py
+++ services/aria.py
@@ -8,6 +8,14 @@ import aiohttp
 import config
 
 
+# 一个简单的用于发送请求的函数
+async def url_json_request():
+    '''Serve ALL aria functions. Give an aria2 request method and some required data.
+    
+    Return: A responese type object'''
+
 # 添加任务
-async def add_task():
-    pass
+async def add_task(info_text):
+    async with aiohttp.ClientSession() as session:
+        async with session.post(config.aria2_url, data=info_text) as resp:
+            return await resp.text()
